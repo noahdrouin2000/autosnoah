8c6cc01192199a2f05ff44a21d52da215f492ea9
noahdrouin2000 <110123451+noahdrouin2000@users.noreply.github.com>
2023-02-11 07:48:20 -0500

    Add files via upload

---
+++ financement.js
@@ -0,0 +1,53 @@
+//GSAP
+const tl = gsap.timeline({ defaults: { duration: 0.75 } });
+
+tl.fromTo("#clock, #map", { y: 0 }, { y: -8, yoyo: true, repeat: -1 });
+
+//Selector
+
+const carsLi = document.querySelectorAll(".cars-li");
+const fourthSectBg = document.querySelector(".fourth-sect");
+
+//Event Listeners
+
+document.addEventListener("mousemove", movingBg);
+for (i = 0; i < carsLi.length; i++) {
+  carsLi[i].addEventListener("mouseover", carHover);
+}
+for (i = 0; i < carsLi.length; i++) {
+  carsLi[i].addEventListener("mouseout", carNotHover);
+}
+
+//Functions
+
+function movingBg(e) {
+  mousePosY = e.clientY;
+  mousePosX = e.clientX;
+  fourthSectBg.style.backgroundPositionX =
+    (mousePosX - window.innerWidth / 2) / 900 + "rem";
+  fourthSectBg.style.backgroundPositionY =
+    (mousePosY - window.innerHeight / 2) / 900 + "rem";
+}
+
+function carHover(e) {
+  let exactParent = e.target.parentNode;
+  exactParent.querySelector("img").style.top = "0.6rem";
+  exactParent.querySelector("h5").style.color = "#ec1d23";
+  exactParent.querySelector(".red-line").style.width = "85%";
+}
+
+function carNotHover(e) {
+  let exactParent = e.target.parentNode;
+  exactParent.querySelector("img").style.top = "1rem";
+  exactParent.querySelector("h5").style.color = "#707070";
+  exactParent.querySelector(".red-line").style.width = "0";
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
